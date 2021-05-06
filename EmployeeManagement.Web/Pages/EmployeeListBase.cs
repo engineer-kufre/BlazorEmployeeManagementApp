41ed1bfcc1d09eee2a283ee468caa53810842cd7
Kufreabasi <engineer_kufre@yahoo.com>
2021-05-06 18:41:32 +0100

    Get Employees using API

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,9 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
         public IEnumerable<Employee> Employees { get; set; }
 
         //protected override async Task OnInitializedAsync()
@@ -20,63 +24,9 @@ namespace EmployeeManagement.Web.Pages
         {
             if (firstRender)
             {
-                await Task.Run(this.LoadEmployees);
+                this.Employees = (await this.EmployeeService.GetEmployees()).ToList();
                 StateHasChanged();
             }
-        }
-
-        private void LoadEmployees()
-        {
-            System.Threading.Thread.Sleep(3000);
-            Employee e1 = new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "John",
-                LastName = "Hastings",
-                Email = "David@pragimtech.com",
-                DateOfBirth = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/image.jpg"
-            };
-
-            Employee e2 = new Employee
-            {
-                EmployeeId = 2,
-                FirstName = "Sam",
-                LastName = "Galloway",
-                Email = "Sam@pragimtech.com",
-                DateOfBirth = new DateTime(1981, 12, 22),
-                Gender = Gender.Male,
-                DepartmentId = 2,
-                PhotoPath = "images/image.jpg"
-            };
-
-            Employee e3 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Mary",
-                LastName = "Smith",
-                Email = "mary@pragimtech.com",
-                DateOfBirth = new DateTime(1979, 11, 11),
-                Gender = Gender.Female,
-                DepartmentId = 1,
-                PhotoPath = "images/image.jpg"
-            };
-
-            Employee e4 = new Employee
-            {
-                EmployeeId = 4,
-                FirstName = "Sara",
-                LastName = "Longway",
-                Email = "sara@pragimtech.com",
-                DateOfBirth = new DateTime(1982, 9, 23),
-                Gender = Gender.Female,
-                DepartmentId = 3,
-                PhotoPath = "images/image.jpg"
-            };
-
-            Employees = new List<Employee> { e1, e2, e3, e4 };
         }
     }
 }
