780aab08fa96f087af5819772eb02aed57e4f6fe
Kufreabasi <engineer_kufre@yahoo.com>
2021-05-06 19:27:21 +0100

    Created Employee detail page

---
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,33 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        public Employee Employee { get; set; }
+
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                this.Id = this.Id ?? "1";
+
+                this.Employee = await this.EmployeeService.GetEmployee(int.Parse(this.Id));
+
+                this.StateHasChanged();
+            }
+        }
+    }
+}
